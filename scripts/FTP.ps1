d5ff28130d08dcc5a5850f5aa7f7e816f2fcae6b
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-05-22 21:12:02 -0700

    setting up transfer for other destination

--- scripts/FTP.ps1
+++ scripts/FTP.ps1
@@ -1,18 +1,50 @@
-
+param([switch]$Default,[switch]$sandbox)
 $creds = Get-Content $PSScriptRoot\..\config\ssh.json|Out-String|ConvertFrom-Json
 [XML]$FTPItems = Get-Content $PSScriptRoot\..\config\Transfer.xml
 
-foreach($item in $FTPItems.Site.Items.Item)
+if($Default)
 {
-  Write-Host "`nTag: $($item.Tag)" -ForegroundColor Green;
-  & "C:\Program Files (x86)\WinSCP\winscp.com" `
-    /log="B:\SOURCES\Repos\site\logs\winscp.log" /ini=nul `
-    /command `
-    "open sftp://$($creds.Username)@$($creds.IP):$($creds.Port)/ -hostkey=`"`"$($creds.HostKey)`"`" -privatekey=`"`"$($creds.sshkeyPath)`"`" -passphrase=`"`"$($creds.Password)`"`"" `
-    "lcd $($item.LCD)" `
-    "cd $($item.CD)" `
-    "put $($item.Package)" `
-    "exit"
+  foreach($dest in $FTPItems.Site.Destination)
+  {
+    if($dest.Name -eq "Default")
+    {
+      [System.Object[]]$Processes = $dest.Processes;
+      foreach($item in $Processes.Process)
+      {
+        Write-Host "`nTag: $($item.Tag)" -ForegroundColor Green;
+        & "C:\Program Files (x86)\WinSCP\winscp.com" `
+          /log="B:\SOURCES\Repos\site\logs\winscp.log" /ini=nul `
+          /command `
+          "open sftp://$($creds.Username)@$($creds.IP):$($creds.Port)/ -hostkey=`"`"$($creds.HostKey)`"`" -privatekey=`"`"$($creds.sshkeyPath)`"`" -passphrase=`"`"$($creds.Password)`"`"" `
+          "lcd $($item.LCD)" `
+          "cd $($item.CD)" `
+          "put $($item.Package)" `
+          "exit"
+      }
+    }
+  }
+}
+if($sandbox)
+{
+  foreach($dest in $FTPItems.Site.Destination)
+  {
+    if($dest.Name -eq "sandbox")
+    {
+      [System.Object[]]$Processes = $dest.Processes;
+      foreach($item in $Processes.Process)
+      {
+        Write-Host "`nTag: $($item.Tag)" -ForegroundColor Green;
+        & "C:\Program Files (x86)\WinSCP\winscp.com" `
+          /log="B:\SOURCES\Repos\site\logs\winscp.log" /ini=nul `
+          /command `
+          "open sftp://$($creds.Username)@$($creds.IP):$($creds.Port)/ -hostkey=`"`"$($creds.HostKey)`"`" -privatekey=`"`"$($creds.sshkeyPath)`"`" -passphrase=`"`"$($creds.Password)`"`"" `
+          "lcd $($item.LCD)" `
+          "cd $($item.CD)" `
+          "put $($item.Package)" `
+          "exit"
+      }
+    }
+  }
 }
 
 $winscpResult = $LastExitCode
