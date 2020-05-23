602f0bd99929fdc6dbe7eb4499dfb7b6feb38248
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-05-22 20:39:14 -0700

    Improving FTP process using xml/json

--- scripts/FTP.ps1
+++ scripts/FTP.ps1
@@ -1,19 +1,19 @@
 
 $creds = Get-Content $PSScriptRoot\..\config\ssh.json|Out-String|ConvertFrom-Json
-& "C:\Program Files (x86)\WinSCP\winscp.com" `
-  /log="B:\SOURCES\Repos\site\logs\winscp.log" /ini=nul `
-  /command `
+[XML]$FTPItems = Get-Content $PSScriptRoot\..\config\Transfer.xml
+
+foreach($item in $FTPItems.Site.Items.Item)
+{
+  Write-Host "`nTag: $($item.Tag)" -ForegroundColor Green;
+  & "C:\Program Files (x86)\WinSCP\winscp.com" `
+    /log="B:\SOURCES\Repos\site\logs\winscp.log" /ini=nul `
+    /command `
     "open sftp://$($creds.Username)@$($creds.IP):$($creds.Port)/ -hostkey=`"`"$($creds.HostKey)`"`" -privatekey=`"`"$($creds.sshkeyPath)`"`" -passphrase=`"`"$($creds.Password)`"`"" `
-    "lcd B:\SOURCES\Repos\Site" `
-    "cd /home/u820870703/domains/brandonmfong.com/public_html" `
-    "put img" `
-    "lcd B:\SOURCES\Repos\Site" `
-    "cd /home/u820870703/domains/brandonmfong.com/public_html" `
-    "put docs" `
-    "lcd B:\SOURCES\Repos\Site\config" `
-    "cd /home/u820870703/domains/brandonmfong.com/public_html/config" `
-    "put environmentcredentials.xml" `
+    "lcd $($item.LCD)" `
+    "cd $($item.CD)" `
+    "put $($item.Package)" `
     "exit"
+}
 
 $winscpResult = $LastExitCode
 if ($winscpResult -eq 0)
